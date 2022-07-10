bc08d072b872d091a594f9a776ca66451f1d7f39
MikiBersa <trombettamiki@gmail.com>
2022-07-10 12:14:50 +0200

    MESSA LA SPIEGAZIONE

--- PF22_Michele_Bersani.c
+++ PF22_Michele_Bersani.c
@@ -1,3 +1,18 @@
+/*
+SPIEGAZIONE DELLA STRUTTRA DEL CODICE:
+
+Strutture dati: 
+    1) un albero RB per memorizzare le parole così limito la complessità per la ricerca della parola tra quelle ammissibili 
+    2) un dizionario per memorizzare i requisiti appresi durante il confronto con la parola di riferimento
+    3) una lista bidirezionale con puntatori alla testa e alla fine della lista per tenere in maniera raggruppata le parole valide cioè con i requisiti appresi con il confronto
+    4) eventuali dizionari per il conteggi dei caratteri all'interno della parola
+
+PROBLEMA:
+    output corretto negli open ma poi va in timeout nelle prove successive 
+    faccio di media 0.066 secondi nel subtask5 dell'open
+    sto cercando un modo per ridurre il tempo di esecuzione che per ora non ho trovato
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
