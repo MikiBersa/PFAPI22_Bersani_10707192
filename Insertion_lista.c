e5283d23ef860efea9547c3bdbcf7371959e6ad1
MikiBersa <trombettamiki@gmail.com>
2022-07-24 15:05:11 +0200

    INSERTION CREEATO ALGORITMO DI BASE IMPLEMENTARLO PER L'ALBERO

--- Insertion_lista.c
+++ Insertion_lista.c
@@ -36,30 +36,90 @@ void inserimento(int num , Root *root){
     NodePtr elem;
     elem = malloc(sizeof(elemento));
     elem->numero = num;
+    elem->next = NULL;
+    elem->prev = NULL;
 
     inserisci_lista(root,elem);
 }
 
 void stampa(NodePtr l){
+    //printf("DENTRO\n");
     while(l!=NULL) {
+        //printf("DENTRO\n");
         printf("%d ", l->numero);
         l = l->next;
     }
 }
 
+void insertion_sort(Root * root){
+    NodePtr curr = root->radice_lista->next;
 
+    while(curr!=NULL){
+        NodePtr i = curr->prev;
+        NodePtr dopo = curr->next;
+        if(curr->numero < i->numero){ 
+            curr = dopo;
+            continue;
+        }
+        while(i!=NULL && curr->numero > i->numero){
+            i = i->prev;
+        }
+        //cancellazione
+        curr->prev->next = curr->next;
+        if(curr->next != NULL) curr->next->prev = curr->prev;
+        else root->fine_lista = curr->prev;
 
+        //inserimento
+        if(i!=NULL){
+            curr->next=i->next;
+            curr->prev = i;
+            i->next->prev = curr;
+            i->next = curr;
+        }else{
+            curr->next = root->radice_lista;
+            curr->prev = NULL;
+            root->radice_lista->prev = curr;
+            root->radice_lista = curr;
+        }
+        curr = dopo;
+    }
+
+    return;
+}
 
 int main(){
     Root root = {NULL,NULL};
+    /*
     inserimento(5,&root);
     inserimento(7,&root);
     inserimento(8,&root);
     inserimento(2,&root);
     inserimento(3,&root);
     inserimento(1,&root);
+    */
+    /*
+    inserimento(4,&root);
+    inserimento(5,&root);
+    inserimento(7,&root);
+    inserimento(1,&root);
+    inserimento(2,&root);
+    inserimento(3,&root);
+    inserimento(8,&root);
+    inserimento(9,&root);
+    inserimento(10,&root);
+    */
+    int i = 0;
+    for(i=0;i<1000;i++){
+        inserimento(i,&root);
+    }
+    
 
     printf("STAMPO LA LISTA\n");
+    stampa(root.radice_lista);
+    printf("\nFACCIO INSERTION\n");
+    insertion_sort(&root);
+
+    printf("\nSTAMPO LA LISTA\n");
     stampa(root.radice_lista);
     return 0;
 }
